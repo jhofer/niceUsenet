d643380dbf04060a3982e9002b0633f14e165c0d
Jonas Hofer <jonas.hofer@outlook.com>
2015-02-15 17:55:44 +0100

    added repeate to scritp

--- cronScript.sh
+++ cronScript.sh
@@ -1,12 +1,12 @@
+
 while true; do
   sudo rm /var/lib/mongodb/mongod.lock
   sudo service mongod start
   nodejs niceUsenet.js &
-  sleep 1h
   last_pid=$!
-  kill -KILL $last_pid
+  sleep 30m
   if ps -p $last_pid -o comm= | grep -qs '^niceUsenet$'; then
           kill -$signal $last_pid 2> /dev/null
-          sleep 1m
+          sleep 10s
   fi
 done
