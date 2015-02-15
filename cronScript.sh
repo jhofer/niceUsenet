5bf822e4918b1ee647cccb64f337783a3619909b
Jonas Hofer <jonas.hofer@outlook.com>
2015-02-15 17:49:28 +0100

    script fix

--- cronScript.sh
+++ cronScript.sh
@@ -1,9 +1,11 @@
-sudo rm /var/lib/mongodb/mongod.lock
-sudo service mongod start
-nodejs niceUsenet.js &
-last_pid=$!
-kill -KILL $last_pid
-if ps -p $last_pid -o comm= | grep -qs '^niceUsenet$'; then
-        kill -$signal $last_pid 2> /dev/null
-        sleep 1h
-fi
+while true; do
+  sudo rm /var/lib/mongodb/mongod.lock
+  sudo service mongod start
+  nodejs niceUsenet.js &
+  last_pid=$!
+  kill -KILL $last_pid
+  if ps -p $last_pid -o comm= | grep -qs '^niceUsenet$'; then
+          kill -$signal $last_pid 2> /dev/null
+          sleep 1h
+  fi
+done
